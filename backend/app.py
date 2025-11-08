9ed68684651e077a44c6ca2400ef88a2a1931120
Md Rashedul Hasan <mhasan6@huskers.unl.edu>
2025-11-08 12:59:56 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -44,4 +44,6 @@ def get_examples():
     return jsonify(DEMO_EXAMPLES), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
